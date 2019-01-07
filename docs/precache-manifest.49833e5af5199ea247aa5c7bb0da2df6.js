c6a44d72f0ea98cfde5d741322e4e14cd0429178
chicainoportuna <paula.mara@gmail.com>
2019-01-07 13:17:36 +0100

    Add to build docs

---
+++ docs/precache-manifest.49833e5af5199ea247aa5c7bb0da2df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo.0118e311.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "657e3a0c6e39fb3d8846",
+    "url": "./static/js/main.657e3a0c.chunk.js"
+  },
+  {
+    "revision": "ae54f64a87739d71cd60",
+    "url": "./static/js/1.ae54f64a.chunk.js"
+  },
+  {
+    "revision": "657e3a0c6e39fb3d8846",
+    "url": "./static/css/main.700de0ce.chunk.css"
+  },
+  {
+    "revision": "86410c35aa511d177f060492bf6c38f5",
+    "url": "./index.html"
+  }
+];
