bf822ced9c5c1a9320d54dc6635b9268f01cd7df
chicainoportuna <paula.mara@gmail.com>
2019-01-07 13:00:28 +0100

    Add index icons

--- src/main_index.js
+++ src/main_index.js
@@ -4,6 +4,14 @@ import React, {
 import './stylesheets/index.scss';
 import logoAdalab from './images/logo-adalab.png';
 import logoCards from './images/logo.svg';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faKeyboard} from '@fortawesome/free-solid-svg-icons';
+import {faShareAlt} from '@fortawesome/free-solid-svg-icons';
+import {faObjectUngroup} from '@fortawesome/free-solid-svg-icons';
+library.add(faShareAlt);
+library.add(faKeyboard);
+library.add(faObjectUngroup);
 
 class Page extends Component {
     render() { 
@@ -20,11 +28,11 @@ class Page extends Component {
                     <h1 className="main__title">Crea tu tarjeta de visita</h1>
                     <p className="main__subtitle">Crea mejores contactos profesionales de forma fácil y cómoda</p>
                     <ul className="main__icons">
-                        <li className="main__item diseña"><i className="icon far fa-object-ungroup"></i>Diseña</li>
-                        <li className="main__item rellena"><i className="icon far fa-keyboard"></i>Rellena</li>
-                        <li className="main__item comparte"><i className="icon fas fa-share-alt"></i>Comparte</li>
+                        <li className="main__item diseña"> <FontAwesomeIcon icon="object-ungroup" className="icon object-ungroup" /> Diseña</li>
+                        <li className="main__item rellena"><FontAwesomeIcon icon="keyboard" className="icon keyboard" /> Rellena</li>
+                        <li className="main__item comparte"><FontAwesomeIcon icon="share-alt" className="icon share-alt" /> Comparte</li>
                     </ul>
-                    <a className="link__other" href="index-edit.html">
+                    <a className="link__other" href="">
                         <div className="btn__comenzar">Comenzar</div>
                     </a>
                 </div>
@@ -34,7 +42,7 @@ class Page extends Component {
             <footer className = "page__footer">
                 <div className = "footer__container">
                      <p className = "footer__copy"> Awesome profile - cards @2019 </p> 
-                    <a href = "https://adalab.es/" target = "_blank"> 
+                    <a href = "https://adalab.es/" target = "_blank " rel="noopener noreferrer"> 
                     <img src = {logoAdalab} alt = "logo-adalab" className = "footer__logo" /> 
                     </a> 
                 </div> 
