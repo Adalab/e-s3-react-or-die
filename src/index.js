cf647df9dda81552af5b0f021fe89fe9fc5b18c5
alhana-yo <beatriz.german.rengel@gmail.com>
2019-01-08 12:20:55 +0100

    Implementing CardIndex in React. Not finished

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.scss';
 import Page from './main_index';
+import CardIndex from './CardIndex';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+ReactDOM.render(<CardIndex />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
