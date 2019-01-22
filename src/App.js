ad7e1d20984a4500802f1a8f6d05d5ae78f3c8cb
alhana-yo <beatriz.german.rengel@gmail.com>
2019-01-22 12:06:46 +0100

    Routing finished

--- src/App.js
+++ src/App.js
@@ -131,22 +131,12 @@ class App extends React.Component {
     render() {
         return (
 
-            <React.Fragment>
-                {/* <CardIndex handleName={this.handleName}
-                handleJob={this.handleJob} handlePhone={this.handlePhone} handleEmail={this.handleEmail} handleLinkedin={this.handleLinkedin} handleGithub={this.handleGithub} handlePhoto={this.handlePhoto} cardInfo={this.state.card} handleUrl={this.handleUrl} handleColor={this.handleColor} colors={this.state.colors} handleTypo={this.handleTypo} typo={this.state.typo}/> */}
+            <Switch>
+                <Route exact path="/" component={MainIndex} />
+                <Route path="/CardIndex" render={() => <CardIndex handleName={this.handleName}
+                    handleJob={this.handleJob} handlePhone={this.handlePhone} handleEmail={this.handleEmail} handleLinkedin={this.handleLinkedin} handleGithub={this.handleGithub} handlePhoto={this.handlePhoto} cardInfo={this.state.card} handleUrl={this.handleUrl} handleColor={this.handleColor} colors={this.state.colors} handleTypo={this.handleTypo} typo={this.state.typo} />} />
 
-                <MainIndex handleName={this.handleName}
-                    handleJob={this.handleJob} handlePhone={this.handlePhone} handleEmail={this.handleEmail} handleLinkedin={this.handleLinkedin} handleGithub={this.handleGithub} handlePhoto={this.handlePhoto} cardInfo={this.state.card} handleUrl={this.handleUrl} handleColor={this.handleColor} colors={this.state.colors} handleTypo={this.handleTypo} typo={this.state.typo} />
-
-
-                <Switch>
-                    <Route exact path="/" component={MainIndex} />
-                    <Route path="/CardIndex" render={() => <CardIndex handleName={this.handleName}
-                        handleJob={this.handleJob} handlePhone={this.handlePhone} handleEmail={this.handleEmail} handleLinkedin={this.handleLinkedin} handleGithub={this.handleGithub} handlePhoto={this.handlePhoto} cardInfo={this.state.card} handleUrl={this.handleUrl} handleColor={this.handleColor} colors={this.state.colors} handleTypo={this.handleTypo} typo={this.state.typo} />} />
-
-                </Switch>
-            </React.Fragment>
-
+            </Switch>
 
         );
     }
